4c47c01da3b7058a8c52681e53debdacdba68dea
Aditya <adityawantstoread@gmail.com>
2023-03-20 13:38:07 +0800

    Officially finished with the whole Tic-Tac Toe, wasn't working for a long time but finally it did because apparently I had to give it a class of tile

--- script.js
+++ script.js
@@ -1,89 +1,124 @@
-let sqaures = Array.from(document.querySelectorAll(".data"));
-let playerDisplay = document.querySelector(".display-player");
-let resetbtn = document.querySelector(".restart");
-let announcementwinner = document.querySelector(".result");
+window.addEventListener('DOMContentLoaded', () => {
+    const tiles = Array.from(document.querySelectorAll('.tile'));
+    const playerDisplay = document.querySelector('.display-player');
+    const resetButton = document.querySelector('#reset');
+    const announcer = document.querySelector('.announcer');
 
-let board = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let GameOn = true;
+    let board = ['', '', '', '', '', '', '', '', ''];
+    let currentPlayer = 'X';
+    let isGameActive = true;
 
-const playerX_WON = 'playerX_WON';
-const platyerO_WON = 'playerO_WON';
-const TIE = 'TIE';
+    const PLAYERX_WON = 'PLAYERX_WON';
+    const PLAYERO_WON = 'PLAYERO_WON';
+    const TIE = 'TIE';
 
-const winningConditions = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-];
 
-function isValid(sqaure) {
-    if (sqaure.innerText === "X" || sqaure.innerText === "O") {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
+    /*
+        Indexes within the board
+        [0] [1] [2]
+        [3] [4] [5]
+        [6] [7] [8]
+    */
 
-function updateBoard(index) {
-    board[index] = currentPlayer;
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-}
-
-function changePlayer() {
-    playerDisplay.classList.remove(`player${currentPlayer}`);
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-    playerDisplay.innerText === currentPlayer;
-    playerDisplay.classList.add(`player${currentPlayer}`);
-}
-
-function announce(type) {
-    switch(type) {
-        case playerO_WON :
-            announcementwinner.innerHTML = 'Player <span class="playerO">O</span> Won';
-            break;
-        case playerX_WON:
-            announcementwinner.innerHTML = 'Player <span class="playerX">X</span> Won';
-        case TIE:
-            announcementwinner.innerHTML = 'TIE';
-    }
-    announcementwinner.classList.remove('hide');
-}
-
-function resultCheck() {
-    let roundWon = false;
-    for (let i=0; i <= 9; i++) {
-        const winCondition = winningConditions[i];
-        const a = board[winCondition[0]];
-        const b = board[winCondition[1]];
-        const c = board[winCondition[2]];
-        if (a === "" || b === "" || c === "") {
-            continue
+    function handleResultValidation() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const a = board[winCondition[0]];
+            const b = board[winCondition[1]];
+            const c = board[winCondition[2]];
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+            if (a === b && b === c) {
+                roundWon = true;
+                break;
+            }
         }
-        if (a === b && b === c){
-            roundWon = true;
-            break;
-        }
-    }
+
     if (roundWon) {
-        announce(currentPlayer === "X" ? playerX_WON : platyerO_WON);
-        GameOn = false;
-        return;
+            announce(currentPlayer === 'X' ? PLAYERX_WON : PLAYERO_WON);
+            isGameActive = false;
+            return;
+        }
+
+    if (!board.includes(''))
+        announce(TIE);
     }
 
-    if (!board.includes("")) announce(TIE);
-}
+    const announce = (type) => {
+        switch(type){
+            case PLAYERO_WON:
+                announcer.innerHTML = 'Player <span class="playerO">O</span> Won';
+                break;
+            case PLAYERX_WON:
+                announcer.innerHTML = 'Player <span class="playerX">X</span> Won';
+                break;
+            case TIE:
+                announcer.innerText = 'Tie';
+        }
+        announcer.classList.remove('hide');
+    };
 
-function UserAction(sqaure, index) {
-    if (isValid(sqaure) && GameOn) {
-        sqaure.innerText = currentPlayer;
-        sqaure.classList.add(`player${currentPlayer}}`);
-        updateBoard(index);
+    const isValidAction = (tile) => {
+        if (tile.innerText === 'X' || tile.innerText === 'O'){
+            return false;
+        }
+
+        return true;
+    };
+
+    const updateBoard =  (index) => {
+        board[index] = currentPlayer;
     }
-}
+
+    const changePlayer = () => {
+        playerDisplay.classList.remove(`player${currentPlayer}`);
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        playerDisplay.innerText = currentPlayer;
+        playerDisplay.classList.add(`player${currentPlayer}`);
+    }
+
+    const userAction = (tile, index) => {
+        if(isValidAction(tile) && isGameActive) {
+            tile.innerText = currentPlayer;
+            tile.classList.add(`player${currentPlayer}`);
+            updateBoard(index);
+            handleResultValidation();
+            changePlayer();
+        }
+    }
+    
+    const resetBoard = () => {
+        board = ['', '', '', '', '', '', '', '', ''];
+        isGameActive = true;
+        announcer.classList.add('hide');
+
+        if (currentPlayer === 'O') {
+            changePlayer();
+        }
+
+        tiles.forEach(tile => {
+            tile.innerText = '';
+            tile.classList.remove('playerX');
+            tile.classList.remove('playerO');
+        });
+    }
+
+    tiles.forEach( (tile, index) => {
+        tile.addEventListener('click', () => userAction(tile, index));
+    });
+
+    resetButton.addEventListener('click', resetBoard);
+});
